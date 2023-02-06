6d0cd81138f4dc6bef2df052e00ef5dee8731757
marcelohumerez <marcelohumerez@gmail.com>
2023-02-06 08:09:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newsdata';
+  //title = 'newsdata';
+
+  listNoticias: any[] = [];
+  loading = false;
+
+  constructor(private _noticiaService: NoticiaService){
+
+  }
+
+
+  buscarNoticias(parametros: any) {
+   // console.log('Soy el padre');
+   // console.log(parametros);
+
+    this.loading = true;
+    this.listNoticias = [];
+
+    setTimeout(() => {
+      this._noticiaService.getNoticias(parametros).subscribe(data => {
+        //console.log(data);
+        this.loading = false;
+        this.listNoticias = data.results;
+      }, error => {
+        console.log(error);
+        this.loading = false;
+      })
+    }, 1000);
+
+
+  }
 }
